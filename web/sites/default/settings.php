74c7bf45362cf30ddc8965064a90899ce6291224
Eric Michalsen <eric.michalsen@pantheon.io>
2021-12-22 14:38:11 -0500

    redis engage

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -34,7 +34,6 @@ if (file_exists($local_settings)) {
 }
 
 // Configure Redis
-
 if (defined('PANTHEON_ENVIRONMENT')) {
   // Include the Redis services.yml file. Adjust the path if you installed to a contrib or other subdirectory.
   $settings['container_yamls'][] = 'modules/redis/example.services.yml';
